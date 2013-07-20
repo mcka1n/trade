a807c3af7430872231cdabfddf8fa50bb76f53df
Ed Mckain <mckain.default@gmail.com>
2013-07-20 16:36:57 -0600

    creates a brande new rails 3.2.13 project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trade::Application.config.secret_token = '43fef0c5d522b00173b166754a02c373b78c97351f7512d5f5f37f8e84a93ad133f7b6950f9691832bfbfdec23ebf26dd27b6e024abee5a83f3b7137a34fafdb'
