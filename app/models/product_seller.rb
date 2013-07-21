8d0aa854d4d752e1bc133da0f695dda2908b7b39
Ed Mckain <mckain.default@gmail.com>
2013-07-21 13:23:33 -0600

    adds more fields to ProductSeller model in order to complete the whole project.

--- app/models/product_seller.rb
+++ app/models/product_seller.rb
@@ -6,9 +6,14 @@ class ProductSeller
 
   field :product_id, type: String
   field :name, type: String
-  field :price, type: String
+  field :rating, type: String
+  field :condition, type: String
+  field :tax, type: String
+  field :base_price, type: String
+  field :total_price, type: String
   field :image, type: String
   field :description, type: String
+  field :url, type: String
 
   belongs_to :product
 end
