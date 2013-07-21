b929b492264d1cc3ad71c50814e54b284e7f810e
Ed Mckain <mckain.default@gmail.com>
2013-07-20 18:28:34 -0600

    adds a simple search form.

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,12 @@ class Product
   field :name, type: String
   field :description, type: String
 
+  def self.search(search)
+    if search
+      Product.all.where gtin: /#{search}/i
+    else
+      Product.all
+    end
+  end
 
 end
