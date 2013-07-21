faf55f2202249227311cbad62e314476c351e046
Ed Mckain <mckain.default@gmail.com>
2013-07-21 15:04:25 -0600

    tries to avoid issues with empty response from Google.

--- app/models/product_seller.rb
+++ app/models/product_seller.rb
@@ -2,7 +2,7 @@ class ProductSeller
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  attr_accessible :product_id, :name, :price, :image, :description
+  attr_accessible :product_id, :name, :rating, :condition, :tax, :base_price, :total_price, :description, :url
 
   field :product_id, type: String
   field :name, type: String
@@ -11,7 +11,7 @@ class ProductSeller
   field :tax, type: String
   field :base_price, type: String
   field :total_price, type: String
-  field :image, type: String
+  #field :image, type: String
   field :description, type: String
   field :url, type: String
 
--- app/models/source.rb
+++ app/models/source.rb
@@ -20,7 +20,14 @@ class Source
     price = ''
     description = '' 
     product_name = ''
+
     seller_name = ''
+    seller_rating = ''
+    url = ''
+    product_condition = ''
+    product_tax = ''
+    total_price = ''
+    base_price = ''
 
     page = agent.get('http://www.google.com/shopping/')
     google_form = page.form('gbqf')
@@ -45,7 +52,49 @@ class Source
         @product = Product.find_or_create_by(:gtin => gtin)
       end
       
-      @product_seller = ProductSeller.find_or_create_by(:product_id => @product._id, :name => seller_name, :price => price, :image => image, :description => description)
+      #@product_seller = ProductSeller.find_or_create_by(:product_id => @product._id, :name => seller_name, :price => price, :image => image, :description => description)
+
+      # #####################################
+      # Clicks and gets data from 2nd page 
+      # #####################################
+      second_page = each_pslires.search("//div[@class='pslimain']//h3//a")[counter]
+      counter_two = 0
+      iteration_two = Mechanize::Page::Link.new(second_page, agent, page).click
+      
+      
+      iteration_two.parser.xpath("//div[@id='main-content-with-search']//div[@id='ps-sellers-content']//div[@class='ps-sellers-table-container']//table//tr").each do |each_root_table_tr|
+        if counter_two >= 1          
+
+          if !each_root_table_tr.search("//tr//td[@class='seller-col']//span")[counter_two].nil?
+            seller_name = each_root_table_tr.search("//tr//td[@class='seller-col']//span")[counter_two].content
+          end
+
+          seller_rating = each_root_table_tr.search("//tr//td[@class='rating-col']")[counter_two]
+          
+          if !each_root_table_tr.search("//tr//td[@class='seller-col']//span//a")[counter_two].nil?
+            url = each_root_table_tr.search("//tr//td[@class='seller-col']//span//a")[counter_two].attributes['href']
+          end
+
+          if !each_root_table_tr.search("//tr//td[@class='condition-col']//span")[counter_two].nil?
+            product_condition = each_root_table_tr.search("//tr//td[@class='condition-col']//span")[counter_two].content
+          end
+
+          if !each_root_table_tr.search("//tr//td[@class='taxship-col']//span")[counter_two].nil?
+            product_tax = each_root_table_tr.search("//tr//td[@class='taxship-col']//span")[counter_two].content
+          end
+
+          if !each_root_table_tr.search("//tr//td[@class='total-col']")[counter_two].nil?
+            total_price = each_root_table_tr.search("//tr//td[@class='total-col']")[counter_two].content
+          end
+
+          if !each_root_table_tr.search("//tr//td[@class='price-col']//span")[counter_two].nil?
+            base_price = each_root_table_tr.search("//tr//td[@class='price-col']//span")[counter_two].content
+          end
+
+          @product_seller = ProductSeller.find_or_create_by(:product_id => @product._id, :name => seller_name, :rating => seller_rating, :condition => product_condition, :tax => product_tax, :base_price => base_price, :total_price => total_price, :url => url)  
+        end
+        counter_two = counter_two + 1
+      end
 
       counter = counter + 1
     end
